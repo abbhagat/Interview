70d254e5a2386741d230d5a97b92140943a38ad3
abbhagat <abhinaw.kumar7@gmail.com>
2021-02-21 10:50:27 +0530

    IntelliJ

--- DataStructure/src/geeks/CountPairsWithSumDivisibleByK.java
+++ DataStructure/src/geeks/CountPairsWithSumDivisibleByK.java
@@ -11,6 +11,10 @@ import java.util.stream.IntStream;
  * <p>
  * Input : A[] = {5, 9, 36, 74, 52, 31, 42}, k = 3
  * Output : 7
+ * An efficient approach is to use Hashing technique. We will separate elements into buckets depending on their (value mod K).
+ * When a number is divided by K then the remainder may be 0, 1, 2, upto (k-1).
+ * So take an array say freq[] of size K (initialized with Zero) and increase the value of freq[A[i]%K]
+ * so that we can calculate the number of values giving remainder j on division with K.
  */
 
 public class CountPairsWithSumDivisibleByK {
